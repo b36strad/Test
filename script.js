95461de85e116f1de1461f3c235a011d6194bb12
Ryan Via <3825849+b36strad@users.noreply.github.com>
2020-12-18 12:17:45 -0500

    Add files via upload
    Upload JavaScript file

---
+++ script.js
@@ -0,0 +1,10 @@
+var testing = "testing I\'m here"
+
+//<<<<<<< ours
+//console.log(testing)
+//console.log('hello')
+//=======
+//console.log(testing!!!)
+//>>>>>>> theirs
+
+document.write('hello')
